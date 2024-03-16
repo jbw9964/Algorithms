b8e575ed6901cd582b9e9bf889fcd257c3d3bbc7
청주는사과아님 <jbw9964@gmail.com>
2024-03-16 15:59:13 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 216 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -7,44 +7,57 @@ import java.util.StringTokenizer;
 
 public class Main {
     
-    private static final BufferedReader br = new BufferedReader(
+    private static BufferedReader br = new BufferedReader(
         new InputStreamReader(System.in)
     );
-    private static final BufferedWriter bw = new BufferedWriter(
+    private static BufferedWriter bw = new BufferedWriter(
         new OutputStreamWriter(System.out)
     );
+    private static String lineString = null;
+
+    private static int row_size, col_size;
+    private static int[][] arrayA = null;
+    private static int[][] arrayB = null;
 
     public static void main(String[] args) throws IOException {
         
-        String line = br.readLine();
-        StringTokenizer tokenizer = new StringTokenizer(line);
+        Main.readInput();
 
-        int N = Integer.parseInt(tokenizer.nextToken());
-        int M = Integer.parseInt(tokenizer.nextToken());
-
-        int[][] array = new int[N][M];
-
-        for (int count = 0; count < 2; count++) {
-
-            for (int i = 0; i < N; i++) {
-                line = br.readLine();
-                tokenizer = new StringTokenizer(line);
-
-                for (int j = 0; j < M; j++) {
-                    array[i][j] += Integer.parseInt(tokenizer.nextToken());
-                }
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                bw.write(String.valueOf(array[i][j]) + " ");
+        for (int i = 0; i < row_size; i++) {
+            for (int j = 0; j < col_size; j++) {
+                bw.write(arrayA[i][j] + arrayB[i][j] + " ");
             };  bw.newLine();
         }
 
         bw.flush();
-        // bw.close();
-        // br.close();
     }
 
+    public static void readInput() throws IOException {
+        lineString = br.readLine();
+        
+        StringTokenizer temp_Tokenizer = new StringTokenizer(lineString);
+        row_size = Integer.parseInt(temp_Tokenizer.nextToken());
+        col_size = Integer.parseInt(temp_Tokenizer.nextToken());
+        
+        arrayA = new int[row_size][col_size];
+        arrayB = new int[row_size][col_size];
+        
+        for (int i = 0; i < row_size; i++) {
+            lineString = br.readLine();
+            String[] rowString = lineString.split(" ");
+
+            for (int j = 0; j < col_size; j++) {
+                arrayA[i][j] = Integer.parseInt(rowString[j]);
+            }
+        }
+
+        for (int i = 0; i < row_size; i++) {
+            lineString = br.readLine();
+            String[] rowString = lineString.split(" ");
+
+            for (int j = 0; j < col_size; j++) {
+                arrayB[i][j] = Integer.parseInt(rowString[j]);
+            }
+        }
+    }
 }
