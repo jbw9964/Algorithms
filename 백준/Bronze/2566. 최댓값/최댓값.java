8d2196f17f145f20fcf58ca5ea13cf3e8874ef7d
청주는사과아님 <jbw9964@gmail.com>
2024-03-16 16:18:09 +0900

    [Bronze III] Title: 최댓값, Time: 156 ms, Memory: 16232 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,9 +8,7 @@ public class Main {
     private static BufferedReader br = new BufferedReader(
         new InputStreamReader(System.in)
     );
-    private static BufferedWriter bw = new BufferedWriter(
-        new OutputStreamWriter(System.out)
-    );
+    
     private static String lineString = null;
 
     public static void main(String[] args) throws IOException {
@@ -34,8 +30,6 @@ public class Main {
             }
         }
 
-        bw.write(String.valueOf(maxima) + "\n");
-        bw.write(String.valueOf(maximumIndex[0]) + " " + maximumIndex[1] + "\n");
-        bw.flush();
+        System.out.println(maxima + "\n" + maximumIndex[0] + " " + maximumIndex[1]);
     }
 }
