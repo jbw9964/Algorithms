2c013989fb5d19ee022b10ef61ff4e1185118c0a
청주는사과아님 <jbw9964@gmail.com>
2024-03-17 14:38:41 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    private static final BufferedReader br = new BufferedReader(
+        new InputStreamReader(System.in)
+    );
+
+    private static StringBuilder STR = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < N; i++) {
+            STR.append("*".repeat(i + 1))
+                .append("\n");
+        }
+
+        System.out.println(STR.toString());
+    }
+}
