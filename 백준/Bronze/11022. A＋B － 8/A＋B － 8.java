804dff5eaf4e8d3a84a0664915ee5663ccf416a1
청주는사과아님 <jbw9964@gmail.com>
2024-03-17 16:11:39 +0900

    [Bronze V] Title: A+B - 8, Time: 208 ms, Memory: 15280 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    private static final BufferedReader br = new BufferedReader(
+        new InputStreamReader(System.in)
+    );
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+        
+        String[] inpuStrings;
+        int num1, num2;
+
+        for (int i = 1; i <= N; i++) {
+            inpuStrings = br.readLine().split(" ");
+            num1 = Integer.parseInt(inpuStrings[0]);
+            num2 = Integer.parseInt(inpuStrings[1]);
+
+            System.out.printf(
+                "Case #%d: %d + %d = %d\n", 
+                i, num1, num2, num1 + num2
+            );
+        }
+    }
+}
