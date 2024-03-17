aa3e48fb612e89a7bfe2b61b418ee09905088aab
청주는사과아님 <jbw9964@gmail.com>
2024-03-17 16:18:55 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    
+    private static final BufferedReader br = new BufferedReader(
+        new InputStreamReader(System.in)
+    );
+
+    private static final BufferedWriter bw = new BufferedWriter(
+        new OutputStreamWriter(System.out)
+    );
+
+    public static void main(String[] args) throws IOException {
+        br.readLine();
+
+        String input;
+        String[] strings;
+        int num1, num2;
+
+        while ((input = br.readLine()) != null) {
+            strings = input.split(" ");
+
+            num1 = Integer.parseInt(strings[0]);
+            num2 = Integer.parseInt(strings[1]);
+
+            bw.write(String.valueOf(num1 + num2) + "\n");
+        }
+
+        bw.flush();
+    }
+}
