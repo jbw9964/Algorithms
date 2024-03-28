b1d990c2055e6daebab930e03200f7c95ff0c201
청주는사과아님 <jbw9964@gmail.com>
2024-03-28 17:10:07 +0900

    [Bronze V] Title: 새싹, Time: 132 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(
+        new InputStreamReader(System.in)
+    );
+
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("         ,r'\"7\n" + 
+                        "r`-_   ,'  ,/\n" + 
+                        " \\. \". L_r'\n" + 
+                        "   `~\\/\n" + 
+                        "      |\n" + 
+                        "      |");
+    }
+}
