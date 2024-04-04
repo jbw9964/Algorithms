ab38072a758644a5712561920ec12e3eb68527c0
청주는사과아님 <jbw9964@gmail.com>
2024-04-04 18:40:09 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 552 ms, Memory: 51208 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -18,23 +18,23 @@ public class Main {
         int N = Integer.parseInt(tokenizer.nextToken());
         int M = Integer.parseInt(tokenizer.nextToken());
 
-        HashMap<String, Integer> hashTableForName = new HashMap<>(N, 1);
-        HashMap<Integer, String> hashTableForOrder = new HashMap<>(N, 1);
+        HashMap<String, Integer> hashMap = new HashMap<>(N, 1);
+        String[] pokemonArray = new String[N];
 
         for (int i = 1; i <= N; i++) {
-            String input = br.readLine();
-            hashTableForName.put(input, i);
-            hashTableForOrder.put(i, input);
+            String name = br.readLine();
+            hashMap.put(name, i);
+            pokemonArray[i - 1] = name;
         }
 
         for (int i = 0; i < M; i++) {
             String input = br.readLine();
 
             if ('0' <= input.charAt(0) && input.charAt(0) <= '9')
-            sb.append(hashTableForOrder.get(Integer.parseInt(input)) + "\n");
+            sb.append(pokemonArray[Integer.parseInt(input) - 1] + "\n");
 
             else
-            sb.append(hashTableForName.get(input) + "\n");
+            sb.append(hashMap.get(input) + "\n");
         }
 
         System.out.println(sb.toString());
