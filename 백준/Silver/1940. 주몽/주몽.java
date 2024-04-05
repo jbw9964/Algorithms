459d23a25953762ae90f087a03bb24d46cf2162b
청주는사과아님 <jbw9964@gmail.com>
2024-04-05 10:12:47 +0900

    [Silver IV] Title: 주몽, Time: 192 ms, Memory: 16428 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -18,21 +18,28 @@ public class Main {
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
 
         int[] array = new int[N];
-        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = 0; i < N; i++)
+        array[i] = Integer.parseInt(tokenizer.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(tokenizer.nextToken());
-            array[i] = num;
-            hashSet.add(num);
-        }
+        Arrays.sort(array);
+
+        int pt1 = 0;
+        int pt2 = N - 1;
 
         int count = 0;
-        for (int i = 0; i < N; i++) {
-            int num = array[i];
+        while (pt1 < pt2) {
+            int sum = array[pt1] + array[pt2];
 
-            if (hashSet.contains(target - num)) count++;
+            if (array[pt1] >= target)   break;
+
+            if (sum < target)       pt1++;
+            else if (sum > target)  pt2--;
+            else {
+                pt1++;
+                count++;
+            }
         }
 
-        System.out.println(count / 2);
+        System.out.println(count);
     }
 }
