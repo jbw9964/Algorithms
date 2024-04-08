98de5e4991ca885fda6eee11226d184be4d85db8
청주는사과아님 <jbw9964@gmail.com>
2024-04-08 10:08:39 +0900

    [Gold II] Title: 피보나치 수 3, Time: 136 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(
+        new InputStreamReader(System.in)
+    );
+
+    private static final int MOD_NUM = 1_000_000;
+
+
+    public static void main (String[] args) throws IOException {
+        long N = Long.parseLong(br.readLine()) % 3_000_000;
+
+        int fn = 1;
+
+        if (N > 1)  {
+            int fn2 = 0;
+            int fn1 = 1;
+
+            long count = 1;
+            while (count++ < N)   {
+                fn = (fn1 + fn2) % MOD_NUM;
+                fn2 = fn1;
+                fn1 = fn;
+            }
+        }
+
+        System.out.println(fn);
+    }
+}
