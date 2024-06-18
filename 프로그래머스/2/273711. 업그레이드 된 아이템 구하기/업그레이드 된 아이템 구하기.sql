318486adfec18148ffb4085bb6ac985add8e6bd0
청주는사과아님 <jbw9964@gmail.com>
2024-06-18 21:17:47 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,25 @@
+WITH RARE_ITEMS AS (
+    SELECT 
+            ITEM_ID
+    FROM    ITEM_INFO
+    WHERE   RARITY LIKE 'RARE'
+)
+
+,   REINFORCED_ITEMS AS (
+    SELECT
+            B.ITEM_ID
+    FROM    RARE_ITEMS  AS A
+    JOIN    ITEM_TREE   AS B
+            ON A.ITEM_ID = B.PARENT_ITEM_ID
+)
+
+SELECT
+        A.ITEM_ID
+    ,   B.ITEM_NAME
+    ,   B.RARITY
+FROM    REINFORCED_ITEMS    AS A
+JOIN    ITEM_INFO           AS B
+        ON A.ITEM_ID = B.ITEM_ID
+
+ORDER
+BY      A.ITEM_ID DESC
