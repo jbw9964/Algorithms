cdc21e09ed87ace69312090250f464e7543d6226
청주는사과아님 <jbw9964@gmail.com>
2024-08-07 18:18:39 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 16.66 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -1,35 +1,40 @@
-
-class Solution {
+public class Solution {
     public long solution(int r1, int r2) {
-        return getBounded(r2) - getUnBounded(r1);
-    }
+        long answer = 0;
 
-    private static long getBounded(long r) {
-        long total = (2 * r + 1) * (2 * r + 1);
+        for (int x = 0; x < r2; x++)
+            answer += getLowerCount(r2, x);
 
-        long x = r;
-        long del = r;
-        while (x-- > 1) {
-            double yCoord = y(r, x);
-            del += r - (long) yCoord;
+        for (int x = 0; x < r1; x++)    {
+            int lower = getLowerCount(r1, x);
+            if (checkBoundary(r1, x))   lower--;
+            answer -= lower;
         }
 
-        return total - 4 * del;
+        return 4 * answer;
     }
 
-    private static long getUnBounded(long r)    {
-        long sum = r - 1;
+    private int getLowerCount(long r, long x) {
+        long ySquare = r * r - x * x;
+        double y = Math.sqrt(ySquare);
 
-        long x = 0;
-        while (x++ < r - 1) {
-            double yCoord = Math.ceil(y(r, x));
-            sum += (long) yCoord - 1;
-        }
-
-        return 4 * sum + 1;
+        return (int) y;
     }
 
-    private static double y(long r, long x)  {
-        return Math.sqrt(r * r - x * x);
+    private boolean checkBoundary(long r, long x) {
+        long ySquare = r * r - x * x;
+        double y = Math.sqrt(ySquare);
+
+        return (long) y * (long) y == ySquare;
     }
 }
+
+//  x^2 + y^2 = r^2;
+//  y^2 = r^2 - x^2
+
+// 0: 0, 1, 2, 3, 4, 5      -> 6
+// 1: 0, 1, 2, 3, 4         -> 5
+// 2: 0, 1, 2, 3, 4         -> 5
+// 3: 0, 1, 2, 3, 4         -> 5
+// 4: 0, 1, 2, 3            -> 4
+// 5: 0                     -> 1
