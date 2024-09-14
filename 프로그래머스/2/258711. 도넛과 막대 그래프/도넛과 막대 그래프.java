ab03b2ed1b5639840a83f6e423821dc12579461a
청주는사과아님 <jbw9964@gmail.com>
2024-09-14 16:31:12 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 34.25 ms, Memory: 200 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -1,97 +1,33 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
 
 class Solution {
-    private static Integer ROOT_NODE;
-    private static final int DONUT = 1;
-    private static final int STICK = 2;
-    private static final int EIGHT = 3;
-
-    private static Map<Integer, Queue<Integer>> adjacentMap;
-
-    private static void init(int[][] edges)  {
-        ROOT_NODE = null;
-        Set<Integer> nodesIndeg = new HashSet<>();
-        Set<Integer> nodesOutdeg = new HashSet<>();
-
-        adjacentMap = new HashMap<>();
-        
-        for (int[] edge : edges)    {
-            int v1 = edge[0];
-            int v2 = edge[1];
-
-            nodesOutdeg.add(v1);
-            nodesIndeg.add(v2);
-
-            if (!adjacentMap.containsKey(v1))   adjacentMap.put(v1, new LinkedList<>());
-            Queue<Integer> adjacents = adjacentMap.get(v1);
-            adjacents.add(v2);
-        }
-
-        nodesOutdeg.removeAll(nodesIndeg);
-        nodesIndeg.addAll(nodesOutdeg);
-
-        for (int node : nodesOutdeg)    {
-            if (adjacentMap.get(node).size() >= 2)  {
-                ROOT_NODE = node;
-                break;
-            }
-        }
-
-        ROOT_NODE = ROOT_NODE == null ? nodesOutdeg.toArray(new Integer[0])[0] : ROOT_NODE;
-    }
+    private static final int N = 1_000_000;
 
     public int[] solution(int[][] edges) {
-        init(edges);
+        int[] vertexOutdeg = new int[N + 1];
+        int[] vertexIndeg = new int[N + 1];
 
-        System.out.println(ROOT_NODE);
-
-        int[] answer = new int[4];
-        answer[0] = ROOT_NODE;
-
-        for (int node : adjacentMap.get(ROOT_NODE))
-        answer[getGraphType(node)]++;
-
-        return answer;
-    }
-
-    private static int getGraphType(int node)   {
-        if (!adjacentMap.containsKey(node))     return STICK;
-
-        int numOfVertex = 1;
-
-        Queue<Integer> queue = new LinkedList<>();
-        for (int neighbor : adjacentMap.get(node))
-        queue.add(neighbor);
-
-        int numOfEdges = queue.size();
-        Set<Integer> visitSet = new HashSet<>(queue);
-
-
-        while (!queue.isEmpty())    {
-            int current = queue.poll();
-
-            if (current == node)    continue;
-
-            numOfVertex++;
-            if (!adjacentMap.containsKey(current))      continue;
-
-            for (int neighbor : adjacentMap.get(current))   {
-                numOfEdges++;
-
-                if (visitSet.contains(neighbor))        continue;
-
-                visitSet.add(neighbor);
-                queue.add(neighbor);
-            }
+        for (int[] edge : edges)    {
+            vertexOutdeg[edge[0]]++;
+            vertexIndeg[edge[1]]++;
         }
 
-        return numOfVertex == numOfEdges ? 
-        DONUT : numOfVertex + 1 == numOfEdges ? 
-        EIGHT : STICK;
+        int root = 0;
+        int numOfSticks = 0;
+        int numOfEights = 0;
+
+        for (int i = 1; i < N; i++)     {
+            if (vertexOutdeg[i] >= 2)   {
+                if (vertexIndeg[i] == 0)    root = i;
+                else                        numOfEights++;
+            }
+            else if (vertexOutdeg[i] == 0 && vertexIndeg[i] >= 1)
+            numOfSticks++;
+        }
+        
+        return new int[] {
+            root, 
+            vertexOutdeg[root] - numOfSticks - numOfEights,
+            numOfSticks, numOfEights
+        };
     }
 }
