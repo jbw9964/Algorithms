8644c31e2abe3d413549616f9491e4cdb47cf406
청주는사과아님 <jbw9964@gmail.com>
2025-02-11 17:22:06 +0900

    [Gold IV] Title: 동전 바꿔주기, Time: 172 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+    );
+
+    private static int T, K;
+
+    private static int[] numOfCasesInCost;
+    private static int[] priceTable, coinNumTable;
+
+    private static void init() throws IOException {
+        T = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+
+        numOfCasesInCost = new int[T + 1];
+
+        priceTable = new int[K];
+        coinNumTable = new int[K];
+
+        for (int i = 0; i < K; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            priceTable[i] = Integer.parseInt(st.nextToken());
+            coinNumTable[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        init();
+
+        numOfCasesInCost[0] = 1;
+
+        for (int i = 0; i < K; i++) {
+
+            int coinPrice = priceTable[i];
+            int maxCoinNum = coinNumTable[i];
+
+            for (int price = T; price > 0; price--) {
+                for (int n = 1; n <= maxCoinNum; n++) {
+                    
+                    int prevPrice = price - coinPrice * n;
+
+                    if (prevPrice < 0)  {
+                        continue;
+                    }
+
+                    numOfCasesInCost[price] += numOfCasesInCost[prevPrice];
+                }
+            }
+        }
+
+        System.out.println(numOfCasesInCost[T]);
+    }
+}
