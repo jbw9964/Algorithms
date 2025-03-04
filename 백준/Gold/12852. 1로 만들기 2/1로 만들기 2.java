48c1c063100dd17b23fb5f48442b895d52e332dc
청주는사과아님 <jbw9964@gmail.com>
2025-03-04 17:09:01 +0900

    [Gold V] Title: 1로 만들기 2, Time: 112 ms, Memory: 22056 KB -BaekjoonHub

---
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+    );
+
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        int[] table = new int[N + 1];
+        Queue<Integer> q = new ArrayDeque<>(N);
+        q.add(N);
+
+        while (!q.isEmpty() && table[1] == 0)    {
+
+            int curr = q.poll();
+
+            int target;
+            if (curr % 3 == 0)  {
+                target = curr / 3;
+
+                if (table[target] == 0)    {
+                    table[target] = curr;
+                    q.add(target);
+                }
+            }
+
+            if (curr % 2 == 0)  {
+                target = curr / 2;
+                if (table[target] == 0)    {
+                    table[target] = curr;
+                    q.add(target);
+                }
+            }
+
+            target = curr - 1;
+            if (target >= 0 && table[target] == 0)    {
+                table[target] = curr;
+                q.add(target);
+            }
+        }
+
+        Deque<Integer> ans = new ArrayDeque<>();
+        ans.add(1);
+
+        int tmp = 1;
+        while (tmp != N)    {
+            ans.add(table[tmp]);
+            tmp = table[tmp];
+        }
+
+        System.out.println(ans.size() - 1);
+        while (!ans.isEmpty()) {
+            System.out.print(ans.pollLast() + " ");
+        }
+        System.out.println();
+    }
+}
