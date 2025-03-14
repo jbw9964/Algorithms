844dc49886599530045b45568113f72bfacdab38
청주는사과아님 <jbw9964@gmail.com>
2025-03-14 18:13:00 +0900

    [Gold III] Title: 앱, Time: 148 ms, Memory: 16788 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -7,26 +7,40 @@ class Main {
             new InputStreamReader(System.in)
     );
 
-    private static final int MAX = 100 * 100;
+    private static int[] bytes, costs;
+    private static int N, M, MAX;
 
-    public static void main(String[] args) throws IOException {
+    private static void init() throws IOException {
         StringTokenizer st1 = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st1.nextToken());
-        int M = Integer.parseInt(st1.nextToken());
+        N = Integer.parseInt(st1.nextToken());
+        M = Integer.parseInt(st1.nextToken());
 
         st1 = new StringTokenizer(br.readLine());
         StringTokenizer st2 = new StringTokenizer(br.readLine());
 
+        bytes = new int[N];
+        costs = new int[N];
+        for (int i = 0; i < N; i++) {
+            bytes[i] = Integer.parseInt(st1.nextToken());
+            costs[i] = Integer.parseInt(st2.nextToken());
+        }
+
+        MAX = Arrays.stream(costs).sum();
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+
         int[] dp = new int[MAX + 1];
 
         for (int i = 0; i < N; i++) {
-            int bytes = Integer.parseInt(st1.nextToken());
-            int cost = Integer.parseInt(st2.nextToken());
+            int currByte = bytes[i];
+            int currCost = costs[i];
 
-            for (int j = MAX; j >= cost; j--) {
-                int prev = dp[j - cost];
-                dp[j] = Math.max(dp[j], prev + bytes);
+            for (int j = MAX; j >= currCost; j--) {
+                int prev = dp[j - currCost];
+                dp[j] = Math.max(dp[j], prev + currByte);
             }
         }
 
