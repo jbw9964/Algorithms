06890fa199dfee11b0abe353a798189ef1db8a70
청주는사과아님 <jbw9964@gmail.com>
2025-03-14 18:04:40 +0900

    [Gold III] Title: 앱, Time: 156 ms, Memory: 16388 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -7,37 +7,31 @@ class Main {
             new InputStreamReader(System.in)
     );
 
-    private static final int MAX = 100 * 100 + 1;
+    private static final int MAX = 100 * 100;
 
     public static void main(String[] args) throws IOException {
-
         StringTokenizer st1 = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st1.nextToken());
         int M = Integer.parseInt(st1.nextToken());
 
-        int[][] dp = new int[N + 1][MAX];
         st1 = new StringTokenizer(br.readLine());
         StringTokenizer st2 = new StringTokenizer(br.readLine());
 
-        for (int i = 1; i <= N; i++) {
+        int[] dp = new int[MAX + 1];
 
-            int givenBytes = Integer.parseInt(st1.nextToken());
-            int givenCost = Integer.parseInt(st2.nextToken());
+        for (int i = 0; i < N; i++) {
+            int bytes = Integer.parseInt(st1.nextToken());
+            int cost = Integer.parseInt(st2.nextToken());
 
-            int[] prev = dp[i - 1];
-            int[] curr = dp[i];
-
-            assert givenCost >= 0;
-
-            System.arraycopy(prev, 0, curr, 0, givenCost);
-
-            for (int j = givenCost; j < MAX; j++) {
-                curr[j] = Math.max(prev[j], prev[j - givenCost] + givenBytes);
+            for (int j = MAX; j >= cost; j--) {
+                int prev = dp[j - cost];
+                dp[j] = Math.max(dp[j], prev + bytes);
             }
         }
 
-        for (int minCost = 0; minCost < MAX; minCost++) {
-            if (dp[N][minCost] >= M) {
+        for (int minCost = 0; minCost <= MAX; minCost++) {
+            if (dp[minCost] >= M) {
                 System.out.println(minCost);
                 System.exit(0);
             }
