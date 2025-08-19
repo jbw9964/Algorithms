ea43e274adc40c50c03bde0a2bb18e4c8791fede
청주는사과아님 <jbw9964@gmail.com>
2025-08-19 21:43:58 +0900

    [Platinum III] Title: 돌 던지기, Time: 700 ms, Memory: 60716 KB -BaekjoonHub

---
+++ 백준/Platinum/3025. 돌 던지기/돌 던지기.java
@@ -0,0 +1,142 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+    );
+
+    private static int R, C;
+    private static char[][] table;
+    private static int N;
+    private static int[] dropColumns;
+    private static Deque<Cord>[] cordCaches;
+
+    private static boolean inRange(int r, int c) {
+        return r>=0 && r<R && c>=0 && c<C;
+    }
+
+    private static void init() throws IOException {
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        table = new char[R][];
+        for (int i = 0; i < R; i++) {
+            table[i] = br.readLine().toCharArray();
+        }
+
+        N = Integer.parseInt(br.readLine());
+        dropColumns = new int[N];
+        for (int i = 0; i < N; i++) {
+            dropColumns[i] = Integer.parseInt(br.readLine()) - 1;
+        }
+
+        //noinspection unchecked
+        cordCaches = IntStream.range(0, C)
+                .mapToObj(c -> new ArrayDeque<>(R))
+                .toArray(Deque[]::new);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        init();
+
+        for (int col : dropColumns) {
+            simulateOn(col);
+        }
+
+        printTable();
+    }
+
+    private static void simulateOn(int dropCol) {
+
+        int r, c;
+
+        Deque<Cord> cordCache = cordCaches[dropCol];
+        validateCache(cordCache);
+
+        if (cordCache.isEmpty()) {
+            r = 0;
+            c = dropCol;
+        }
+        else {
+            r = cordCache.peekLast().r;
+            c = cordCache.peekLast().c;
+        }
+
+        while (true)    {
+
+            if (blockedByWallOrEdge(r, c)) {
+                break;
+            }
+
+            if (table[r + 1][c] == '.') {
+                r++;
+                cordCache.addLast(new Cord(r - 1, c));
+                continue;
+            }
+
+            // left assignable
+            if (
+                    inRange(r + 1, c - 1) &&
+                    table[r][c - 1] == '.' &&
+                    table[r + 1][c - 1] == '.'
+            )   {
+                r++;
+                c--;
+                continue;
+            }
+
+            // right assignable
+            if (
+                    inRange(r + 1, c + 1) &&
+                    table[r][c + 1] == '.' &&
+                    table[r + 1][c + 1] == '.'
+            )   {
+                r++;
+                c++;
+                continue;
+            }
+
+            break;
+        }
+
+        table[r][c] = 'O';
+    }
+
+    private static void validateCache(Deque<Cord> cordCache) {
+        Cord peek;
+
+        while (
+                (peek = cordCache.peekLast()) != null &&
+                table[peek.r][peek.c] != '.'
+        ) {
+            cordCache.removeLast();
+        }
+    }
+
+    private static boolean blockedByWallOrEdge(int r, int c)    {
+        return !inRange(r + 1, c) || table[r + 1][c] == 'X';
+    }
+
+
+    public static void printTable() {
+        for (char[] row : table) {
+            System.out.println(String.valueOf(row));
+        }
+    }
+}
+
+class Cord {
+
+    int r, c;
+
+    public Cord(int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
+}
