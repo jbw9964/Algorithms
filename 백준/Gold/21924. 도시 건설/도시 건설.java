f716b83a2eacd33eb8a697de76237f29e85a539e
청주는사과아님 <jbw9964@gmail.com>
2025-09-11 19:41:33 +0900

    [Gold IV] Title: 도시 건설, Time: 912 ms, Memory: 162700 KB -BaekjoonHub

--- 백준/Gold/21924. 도시 건설/도시 건설.java
+++ 백준/Gold/21924. 도시 건설/도시 건설.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.*;
 
 public class Main {
 
@@ -8,17 +9,18 @@ public class Main {
     );
 
     private static int N, M;
-    private static List<Edge>[] graph;
-    private static long TOTAL_COST, VISIT_CNT;
+    private static int[] parents;
+    private static PriorityQueue<Edge> pq;
+    private static long TOTAL_COST, EDGE_CNT;
 
     private static void init() throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        //noinspection unchecked
-        graph = new List[N];
-        TOTAL_COST = VISIT_CNT = 0;
+        parents = IntStream.range(0, N).toArray();
+        pq = new PriorityQueue<>(M);
+        TOTAL_COST = EDGE_CNT = 0;
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -27,67 +29,62 @@ public class Main {
             int v2 = Integer.parseInt(st.nextToken()) - 1;
             int cost = Integer.parseInt(st.nextToken());
 
-            if (graph[v1] == null)  {
-                graph[v1] = new ArrayList<>();
-            }
-            graph[v1].add(new Edge(v2, cost));
-
-            if (graph[v2] == null)  {
-                graph[v2] = new ArrayList<>();
-            }
-            graph[v2].add(new Edge(v1, cost));
-
+            pq.add(new Edge(v1, v2, cost));
             TOTAL_COST += cost;
         }
-
     }
 
     public static void main(String[] args) throws IOException {
 
         init();
 
-        long mstCost = getMstCost(0);
+        long mstCost = getMstCost();
 
-        System.out.println(VISIT_CNT == N ? TOTAL_COST - mstCost : -1);
+        // N 개의 vertex 로 이루어진 MST 그래프는
+        // 반드시 N - 1 개의 edge 를 가진다.
+
+        System.out.println(EDGE_CNT == N - 1 ? TOTAL_COST - mstCost : -1);
     }
 
-    private static long getMstCost(int start) {
-
-        boolean[] visited = new boolean[N];
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(start, 0));
+    private static long getMstCost() {
 
         long costSum = 0;
-        while (!pq.isEmpty() && VISIT_CNT < N) {
+
+        while (!pq.isEmpty() && EDGE_CNT < N - 1) {
+
             Edge curr = pq.poll();
 
-            int to = curr.to;
-            long cost = curr.cost;
+            int v1 = curr.v1, v2 = curr.v2;
+            int p1 = getParent(v1), p2 = getParent(v2);
 
-            if (visited[to]) {
+            if (p1 == p2)   {
                 continue;
             }
 
-            visited[to] = true;
-            costSum += cost;
-            VISIT_CNT++;
+            int root = Math.min(p1, p2);
+            int child = Math.max(p1, p2);
+            parents[child] = root;
 
-            List<Edge> candidates = graph[to];
-            if (candidates!=null && !candidates.isEmpty()) {
-                pq.addAll(candidates);
-            }
+            costSum += curr.cost;
+            EDGE_CNT++;
         }
 
         return costSum;
     }
+
+    private static int getParent(int v) {
+        return parents[v] == v ? v : (parents[v] = getParent(parents[v]));
+    }
 }
 
 class Edge implements Comparable<Edge> {
-    int to;
+
+    int v1, v2;
     long cost;
 
-    public Edge(int to, long cost) {
-        this.to = to;
+    public Edge(int v1, int v2, long cost) {
+        this.v1 = v1;
+        this.v2 = v2;
         this.cost = cost;
     }
 
