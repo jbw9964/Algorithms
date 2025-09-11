ca9fc1a7150e8640324491292f57ef9f1cc33c52
청주는사과아님 <jbw9964@gmail.com>
2025-09-11 18:59:09 +0900

    [Silver IV] Title: Byte Coin, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/17521. Byte Coin/Byte Coin.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+    );
+
+    private static int N, W;
+    private static int[] prices;
+
+    private static void init() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+
+        prices = new int[N];
+        for (int i = 0; i < N; i++) {
+            prices[i] = Integer.parseInt(br.readLine());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        init();
+
+        long answer = W;
+
+        for (int pivot = 0; pivot < N; pivot++) {
+
+            int currentPrice = prices[pivot];
+            
+            int next = pivot;
+            while (next + 1 < N && prices[next] <= prices[next + 1]) {
+                next++;
+            }
+
+            long buyCnt = answer / currentPrice;
+            long sellProfit = buyCnt * prices[next];
+            long mod = answer % currentPrice;
+
+            answer = sellProfit + mod;
+            pivot = next;
+        }
+
+        System.out.println(answer);
+    }
+}
