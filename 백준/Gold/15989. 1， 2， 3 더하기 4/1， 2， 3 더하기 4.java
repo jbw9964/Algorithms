9acd931e8e7b5b582aa758d0c1bbaa31b523ac82
청주는사과아님 <jbw9964@gmail.com>
2025-09-19 16:30:15 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 112 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.*;
+import java.util.stream.*;
 
 public class Main {
 
@@ -7,21 +9,21 @@ public class Main {
     );
 
     private static final int MAXIMA = 10_000;
-    private static int[][] DP;
+    private static int[] DP;
 
     private static void init() throws IOException {
 
-        DP = new int[MAXIMA + 1][2];
-        DP[2][0] = DP[3][0] = DP[3][1] = 1;
+        DP = new int[MAXIMA + 1];
+        Arrays.fill(DP, 1);
 
-        for (int num = 4; num <= MAXIMA; num++) {
+        for (int num = 2; num <= MAXIMA; num++) {
+            DP[num] += DP[num - 2];
+        }
 
-            DP[num][0] = 1 + DP[num - 2][0];
-            DP[num][1] = 1 + DP[num - 3][0] + DP[num - 3][1];
-
+        for (int num = 3; num <= MAXIMA; num++) {
+            DP[num] += DP[num - 3];
         }
     }
-
 
     public static void main(String[] args) throws IOException {
 
@@ -31,11 +33,8 @@ public class Main {
         StringBuilder answer = new StringBuilder();
 
         while (T-- > 0) {
-
             int N = Integer.parseInt(br.readLine());
-
-            int sum = 1 + DP[N][0] + DP[N][1];
-            answer.append(sum).append("\n");
+            answer.append(DP[N]).append("\n");
         }
 
         System.out.println(answer.toString());
