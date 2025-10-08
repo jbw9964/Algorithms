eac13c0df8260dec5ea7a5f85a0ea7dd1361cdf2
청주는사과아님 <jbw9964@gmail.com>
2025-10-08 20:29:50 +0900

    [Silver II] Title: 제곱수의 합, Time: 136 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -8,40 +8,33 @@ public class Main {
     );
 
     private static int N;
-    private static int[] DP;
 
     private static void init() throws IOException {
+
         N = Integer.parseInt(br.readLine());
 
-        DP = new int[N + 2];
-        DP[1] = 1;
-        DP[2] = 2;
-
-        for (int num = 3; num <= N; num++) {
-
-            if (sqrtAble(num))  {
-                DP[num] = 1;
-                continue;
-            }
-
-            DP[num] = num;
-            for (int square = 1; num - square * square >= 1; square++) {
-                DP[num] = Math.min(
-                        DP[num],
-                        DP[num - square * square] + 1
-                );
-            }
-        }
-    }
-
-    private static boolean sqrtAble(long num)   {
-        long sqrt = (long) Math.sqrt(num);
-        return sqrt * sqrt == num;
     }
 
     public static void main(String[] args) throws IOException {
 
         init();
+
+        int[] DP = new int[N + 1];
+        Arrays.fill(DP, Integer.MAX_VALUE);
+        DP[0] = 0;
+        DP[1] = 1;
+
+        for (int curr = 2; curr <= N; curr++) {
+
+            int sqrt = 1;
+            int prev;
+
+            while ((prev = curr - sqrt * sqrt) >= 0) {
+                DP[curr] = Math.min(DP[curr], DP[prev] + 1);
+                sqrt++;
+            }
+
+        }
 
         System.out.println(DP[N]);
     }
