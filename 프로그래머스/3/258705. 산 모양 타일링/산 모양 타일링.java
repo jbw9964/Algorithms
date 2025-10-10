b159b32e5357ee97bf85b1fdc228e4389f3871e5
청주는사과아님 <jbw9964@gmail.com>
2025-10-10 17:27:28 +0900

    [level 3] Title: 산 모양 타일링, Time: 17.96 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,20 +1,30 @@
+import java.util.*;
 
 class Solution {
+
     private static final int MOD = 10_007;
 
     public int solution(int n, int[] tops) {
-        int[][] table = new int[2][n];
-        table[0][0] = 3 + tops[0];
-        table[1][0] = 2 + tops[0];
+
+        int[][] DP = new int[2][n];
+
+        // bottom = n + 1 case
+        int[] withBottomCase = DP[0];
+
+        // bottom = n case
+        int[] withoutBottomCase = DP[1];
+
+        withBottomCase[0] = 3 + tops[0];
+        withoutBottomCase[0] = 2 + tops[0];
 
         for (int i = 1; i < n; i++) {
-            table[1][i] = table[1][i - 1] + table[0][i - 1] * (1 + tops[i]);
-            table[0][i] = table[0][i - 1] * (1 + tops[i]) + table[1][i - 1] + table[0][i - 1];
+            withBottomCase[i] = (2 + tops[i]) * withBottomCase[i - 1] + withoutBottomCase[i - 1];
+            withoutBottomCase[i] = (1 + tops[i]) * withBottomCase[i - 1] + withoutBottomCase[i - 1];
 
-            table[1][i] %= MOD;
-            table[0][i] %= MOD;
+            withBottomCase[i] %= MOD;
+            withoutBottomCase[i] %= MOD;
         }
 
-        return table[0][n - 1];
+        return withBottomCase[n - 1];
     }
 }
